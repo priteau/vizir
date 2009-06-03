3544ed027675433d891181782c47f8edc6c32a5e
Pierre Riteau <pierre.riteau@gmail.com>
2009-06-03 22:42:19 +0200

    Trying heckle... more tests on humanize_time.

--- test/test_vizir.rb
+++ test/test_vizir.rb
@@ -2,16 +2,19 @@ require 'test_helper'
 
 class VizirTest < Test::Unit::TestCase
   TIMES = [
+    [0,   [0,   "second"]],
     [1,   [1,   "second"]],
     [30,  [30,  "seconds"]],
     [59,  [59,  "seconds"]],
     [60,  [1,   "minute"]],
+    [119, [1,   "minute"]],
     [120, [2,   "minutes"]]
   ]
   def test_humanize_time
     TIMES.each do |seconds, result|
       assert_equal(result, Vizir.humanize_time(seconds))
     end
-    assert_raise(RuntimeError) { Vizir.humanize_time(-1) }
+    e = assert_raise(RuntimeError) { Vizir.humanize_time(-1) }
+    assert_equal("Can't humanize negative period", e.message)
   end
 end
