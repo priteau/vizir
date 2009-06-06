d6d84717fe0633469eb62aee4ad071089497d7a0
Pierre Riteau <pierre.riteau@gmail.com>
2009-06-06 23:44:20 +0200

    Move more code to lib.

--- lib/vizir.rb
+++ lib/vizir.rb
@@ -4,7 +4,7 @@ require 'restclient'
 module Vizir
   APIURL = "https://localhost:8080/oargridapi"
   IGNORED_SITES = ['grenoble-exp', 'grenoble-ext', 'grenoble-obs', 'luxembourg', 'portoalegre']
-  
+
   def Vizir.humanize_time(time)
     fail "Can't humanize negative period" if time < 0
 
@@ -33,7 +33,7 @@ module Vizir
         $sites.delete_if { |site| site['site'] == "#{$1}" }
       else
         raise e
-      end    
+      end
     rescue => e
       if e.respond_to?('http_code')
         puts "Error: #{e.http_code}:\n #{e.response.body}"
@@ -44,13 +44,59 @@ module Vizir
       end
     end
   end
-  
+
   def Vizir.learn_sites(api, ignored_sites)
     return get(api, '/sites').delete_if { |hash| ignored_sites.include?(hash["site"]) }
   end
 
   def Vizir.get_api(uri)
     return RestClient::Resource.new(uri, :timeout => 15)
+  end
+
+  def Vizir.learn_new_jobs(api)
+    $sites.each do |site|
+      site_name = site['site']
+      jobs = Vizir.get(api, "#{site['uri']}/jobs")
+      break if jobs == nil
+      jobs.each do |job|
+        if job['owner'] == $login
+          job_details = Vizir.get(api, "#{job['uri']}")
+          break if job_details == nil
+          if job_details['jobType'] == 'INTERACTIVE'
+            jobid = job_details['Job_Id']
+            # If we don't yet know the job, record it in $jobs
+            if $jobs[jobid].nil?
+              $jobs[jobid] = Vizir::Job.new(jobid, Time.at(Integer(job_details['startTime']) + Integer(job_details['walltime'])), site_name.capitalize)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def Vizir.first_ending_job
+    $jobs.sort{|a,b| a[1].end_time <=> b[1].end_time}.first
+  end
+
+  def Vizir.alert_jobs(api)
+    $jobs.each do |jobid, job|
+      remaining_sec = get_remaining_time(job.end_time)
+      if remaining_sec < FIRST_ALERT_TIME
+        # Check if the job still exists before sending a notification
+        updatedjob = Vizir.get(api, "/sites/#{job.site_name.downcase}/jobs/#{job.id}")
+        if updatedjob['state'] != 'Running'
+          # Job is not running anymore, remove it from the hash
+          $jobs.delete(jobid)
+          next
+        end
+        if remaining_sec < 0
+          $stderr.puts "Error: negative time"
+          next
+        end
+        remaining_time, remaining_time_unit = Vizir.humanize_time(remaining_sec)
+        notify_via_growl(jobid, job.site_name, remaining_time, remaining_time_unit)
+      end
+    end
   end
 
   class Job
@@ -61,7 +107,7 @@ module Vizir
       @end_time = end_time
       @site_name = site_name
     end
-    
+
     def Job.create_from_json(json, site_name)
       return Job.new(json['Job_Id'], Time.at(Integer(json['startTime']) + Integer(json['walltime'])), site_name.capitalize)
     end
